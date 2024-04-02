971c78eb3d99ed8a957f3e655090fcc61a97cd74
Naiti S. Bhatt <naitibhatt@gmail.com>
2024-04-02 17:35:23 +0100

    Create batch_run_freesurfer.sh

---
+++ scripts/03.freesurfer/batch_run_freesurfer.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+################################################################################
+# RUN FREESURFER ON BIDS DATA
+#
+# This step must be run before the data can be fully processed through fMRIPrep
+#
+# The fMRIPrep singularity was installed using the following code:
+# 	singularity build /EBC/processing/singularity_images/fmriprep-23.2.1.simg docker://nipreps/fmriprep:23.2.1
+################################################################################
+
+# usage documentation - shown if no text file is provided or if script is run outside EBC directory
+Usage() {
+    echo
+	echo
+    echo "Usage:"
+    echo "./run_freesurfer.sh <list of subjects> <STUDYNAME>"
+    echo
+    echo "Example:"
+    echo "./run_freesurfer.sh list.txt STUDYNAME"
+    echo
+    echo "list.txt is a file containing the participants to run Freesurfer on:"
+    echo "001"
+    echo "002"
+	echo "..."
+    echo
+	echo
+	echo "This script must be run within the /EBC/ directory on the server due to space requirements."
+	echo "The script will terminiate if run outside of the /EBC/ directory."
+	echo
+    echo "Script created by Manuel Blesa & Melissa Thye and modified by Naiti Bhatt"
+    echo
+    exit
+}
+[ "$1" = "" ] || [ "$2" = "" ] && Usage
+
+# if the script is run outside of the EBC directory (e.g., in home direct
+# define subjects from text document
+subjs=$(cat $1) 
+
+# define study [directory] from text document
+study=$2
+
+# define directories
+projDir=`cat ../../PATHS.txt`
+singularityDir="/home/naitibhatt/ebby-fmri-analysis/singularity_images"
+bidsDir="$projDir/$study/data/BIDS"
+derivDir="/home/naitibhatt/ebby-fmri-analysis/data/$study/derivatives"
+
+# export freesurfer license file location
+export license=/home/naitibhatt/ebby-fmri-analysis/freesurfer.txt
+
+# create derivatives directory if it doesn't exist
+if [ ! -d ${derivDir} ]
+then 
+	mkdir -p ${derivDir}
+fi
+
+# export freesurfer license file location
+export license=/home/naitibhatt/ebby-fmri-analysis/freesurfer.txt
+
+# change the location of the singularity cache ($HOME/.singularity/cache by default, but limited space in this directory)
+export SINGULARITY_TMPDIR=${singularityDir}
+export SINGULARITY_CACHEDIR=${singularityDir}
+unset PYTHONPATH
+
+# prepare some writeable bind-mount points
+export SINGULARITYENV_TEMPLATEFLOW_HOME=${singularityDir}/fmriprep/.cache/templateflow
+
+# display subjects
+echo
+echo "Running Freesurfer via fMRIPrep for..."
+echo "${subjs}"
+
+
+# grab subjid
+NAMES=${subjs//sub-}
+
+# run singularity
+singularity run --cleanenv	\
+${singularityDir}/fmriprep-23.2.1.simg  							\
+${bidsDir} ${derivDir}												\
+participant															\
+--participant-label ${NAMES}											\
+  --skip_bids_validation												\
+  --nthreads 16														\
+  --omp-nthreads 16													\
+  --anat-only															\
+  --output-space MNI152NLin2009cAsym:res-2 T1w						\
+  --derivatives ${derivDir}											\
+  --stop-on-first-crash												\
+  -w ${singularityDir}												\
+  --fs-license-file ${license}  > ${derivDir}/log_freesurfer.txt
+	
+
+# iterate for all subjects in the text file
+# iterate for all subjects in the text file
+for subj in ${subjs[@]}; do
+	# grab subjid
+	NAME=${subj##*-}
+ 
+	# check whether the file already exists
+	if [ ! -f ${derivDir}/sourcedata/freesurfer/sub-${NAME}/mri/aparc+aseg.mgz ]
+	then
+		
+		# move subject report and freesurfer output files to appropriate directories
+		mv ${derivDir}/*dseg.tsv ${derivDir}/sourcedata/freesurfer
+		mv ${derivDir}/sub-${NAME}.html ${derivDir}/sub-${NAME}
+			
+		# give other users permissions to created files
+		#chmod -R a+wrx ${derivDir}/sub-${NAME}
+
+	fi
+
+done
